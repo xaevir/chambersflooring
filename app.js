bf96969781b7ae97b1954620a1a8ae5eaab2f97c
bobby <xaevir@gmail.com>
2013-01-28 12:45:22 -0500

    synthetic page done

--- app.js
+++ app.js
@@ -30,6 +30,10 @@ app.configure('development', function(){
 app.get('/', routes.index);
 app.get('/users', user.list);
 
+app.get('/synthetic', function(req, res) {
+  res.render('synthetic');
+})
+
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
 });
